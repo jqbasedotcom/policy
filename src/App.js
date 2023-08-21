1807142e33547d56db906db758e25c53dbded03b
Ame <mrs.amemelwin@gmail.com>
2023-08-21 21:43:32 +0630

    push

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="mx-[20px]">
+    <div className="mt-[50px]">
       <Privacy />
   </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '6rem',
+          '2xl': '8rem',
+        },
+      }
+    },
   },
   plugins: [],
 }
